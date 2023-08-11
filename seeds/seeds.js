254ad05a3c3be1b1731a1334ee12e44ea65bd3da
Aidan-Windebank <ajwindybanks@gmail.com>
2023-08-11 12:29:23 -0500

    got all initial boilerplate files in with some seed data, will need refactoring

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Category} = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const categoryData = require('./categoryData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +11,10 @@ const seedDatabase = async () => {
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
+  });
+
+  const categories = await Category.bulkCreate(categoryData, {
+  
   });
 
   for (const post of postData) {
