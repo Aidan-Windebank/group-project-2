993a87374b51c641960c0b733b0f0f769b531360
Aidan-Windebank <ajwindybanks@gmail.com>
2023-08-15 13:52:08 -0500

    changed redering routes for handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const categoryRoutes = require('./categoryRoutes');
 
+router.use('/categories', categoryRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
