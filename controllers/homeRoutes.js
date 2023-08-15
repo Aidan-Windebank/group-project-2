3d9796f0ccfafc965e7901ef0852887ec6f0f495
Aidan-Windebank <ajwindybanks@gmail.com>
2023-08-15 10:33:56 -0500

    added a get route for profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -67,6 +67,26 @@ router.get('/main_page', withAuth, async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post}],
+    });
+
+    const user = userData.get({ plain: true });
+    console.log(user)
+    res.render('profile', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/main_page', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
